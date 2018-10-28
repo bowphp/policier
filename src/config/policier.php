8b18db33fc3b025587ebc9a5f6e459c07706e3b0
Franck Dakia <dakiafranck@gmail.com>
2018-10-28 18:35:01 +0000

    update config

--- src/config/policier.php
+++ src/config/policier.php
@@ -1,16 +1,60 @@
 <?php
 
 return [
-    "lifetime" => 3600,
+    /**
+     * Token expiration time
+     */
+    "exp" => 3600,
+
+    /**
+     * Token is usable after this time
+     */
     "nbf" => 60,
+
+    /**
+     * Token was issue
+     */
     "iat" => 60,
+
+    /**
+     * Configures the issuer
+     */
     "iss" => "localhost",
+
+    /**
+     * Configures the audience
+     */
     "aud" => "localhost",
-    "typ": "JWT",
-    "alg" => "HS256", // HS256, HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512,
-    'signkey' => null,
+
+    /**
+     * The type of the token, which is JWT
+     */
+    "typ" => "JWT",
+
+    /**
+     * Hashing algorithm being used
+     * 
+     * HS256, HS384, HS512, RS256, RS384, RS512, ES256, ES384, ES512,
+     */
+    "alg" => "HS256",
+
+    /**
+     * Signature using your
+     */
+    'signkey' => 'papac',
+
+    /**
+     * Signature using your RSA
+     */
     "keychain" => [
+        /**
+         * Path to your private key
+         */
         "private" => null,
+
+        /**
+         * Ptah to your public key
+         */
         "public" => null
     ]
 ];
