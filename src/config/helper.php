f6cb0e0ceb008d3aa1ecdb2643ec7d0e59e8762f
Franck Dakia <dakiafranck@gmail.com>
2018-10-28 23:49:58 +0000

    add helper

---
+++ src/config/helper.php
@@ -0,0 +1,26 @@
+<?php
+
+use Bow\Jwt\Policier;
+
+if (!function_exists('policier')) {
+    /**
+     * Policier Helper
+     *
+     * @return Policier
+     */
+    function policier($action = null, ...$args)
+    {
+        $policier = Policier::getInstance();
+
+        if (is_null($action)) {
+            return $policier;
+        }
+
+        if (!in_array($action, ['decode', 'encode', 'verify', 'validate', 'parse'])) {
+            throw new \BadMethodCallException("Action not define");
+            
+        }
+
+        return call_user_func_array([$policier, $action], $args);
+    }
+}
